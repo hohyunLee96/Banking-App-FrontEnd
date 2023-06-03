cc73d23f28486d63d576fd34e40cb8a3b5c36c1f
tsxgli <89969164+tsxgli@users.noreply.github.com>
2023-06-03 21:28:12 +0200

    fixed getalltransactions

--- src/components/transactions/TransactionList.vue
+++ src/components/transactions/TransactionList.vue
@@ -1,69 +1,89 @@
 <template>
     <section>
-      <div class="admin-panel">
-        <!-- Admin panel content -->
-      </div>
-      <div class="container">
-        <h2 class="mt-3 mt-lg-5">Transactions</h2>
-        <button type="button" class="btn btn-primary mt-3" @click="this.$router.push('/createTransaction');">
-          Create Transaction
-        </button>
-        <div class="row mt-3">
-          <TransactionListItem
-            v-for="transaction in transactions"
-            :key="transaction.transactionId"
-            :transaction="transaction"
-          />
+        <div class="admin-panel">
+            <!-- Admin panel content -->
         </div>
-      </div>
+        <div class="container">
+            <h2 class="mt-3 mt-lg-5">Transactions</h2>
+            <button type="button" class="btn btn-primary mt-3" @click="this.$router.push('/createTransaction');">
+                Create Transaction
+            </button>
+            <div class="mt-5" v-for="date in orderedTransactionDates" :key="date">
+                <h3>{{ formatDate(date) }}</h3>
+                <div class="row mt-3">
+                    <TransactionListItem v-for="transaction in orderedTransactions[date]" :key="transaction.transactionId"
+                        :transaction="transaction" />
+                </div>
+            </div>
+        </div>
     </section>
-  </template>
+</template>
   
-  <script>
-  import axios from '../../axios-auth';
-  import TransactionListItem from "./TransactionListItem.vue";
-  
-  export default {
+<script>
+import axios from '../../axios-auth';
+import TransactionListItem from "./TransactionListItem.vue";
+
+export default {
     name: "TransactionList",
     components: {
-      TransactionListItem,
+        TransactionListItem,
     },
     data() {
-      return {
-        transactions: [],
-      };
+        return {
+            transactions: [],
+        };
     },
     mounted() {
-      this.getAllTransactions();
+        this.getAllTransactions();
+    },
+    computed: {
+        orderedTransactions() {
+            const orderedTransactions = {};
+            this.transactions.forEach(transaction => {
+                const date = new Date(transaction.timeStamp).toDateString();
+                if (!orderedTransactions[date]) {
+                    orderedTransactions[date] = [];
+                }
+                orderedTransactions[date].push(transaction);
+            });
+            return orderedTransactions;
+        },
+        orderedTransactionDates() {
+            return Object.keys(this.orderedTransactions).sort();
+        },
     },
     methods: {
-      getAllTransactions() {
-        axios
-          .get("transactions")
-          .then((result) => {
-            console.log(result);
-            this.transactions = result.data;
-          })
-          .catch((error) => console.log(error));
-      },
+        getAllTransactions() {
+            axios
+                .get("transactions")
+                .then((result) => {
+                    console.log(result);
+                    this.transactions = result.data;
+                })
+                .catch((error) => console.log(error));
+        },
+        formatDate(dateString) {
+            const options = { year: 'numeric', month: 'long', day: 'numeric' };
+            return new Date(dateString).toLocaleDateString(undefined, options);
+        },
     },
-  };
-  </script>
+};
+</script>
   
-  <style>
-  .admin-panel {
+<style>
+.admin-panel {
     width: 200px;
     float: left;
-  }
-  
-  .container {
+}
+
+.container {
     margin-left: 200px;
-  }
-  
-  .container::after {
+}
+
+.container::after {
     content: "";
     display: table;
     clear: both;
-  }
-  </style>
+}
+</style>
   
