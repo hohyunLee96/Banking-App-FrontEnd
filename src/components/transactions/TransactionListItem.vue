9ea107808ff58f12c442726bb7520c575b8d35de
tsxgli <89969164+tsxgli@users.noreply.github.com>
2023-06-03 15:38:47 +0200

    fixed add new transactions temporarily

---
+++ src/components/transactions/TransactionListItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xxl-3 p-2">
+      <div class="card product-card h-100">
+        <div class="card-body">
+          <div class="float-start">
+            <p>{{ transaction.accountId }}</p>
+            <p>{{ account.user.firstName + " " + account.user.lastName}}</p>
+            <p>
+              <small>{{ account.iban }}</small>
+            </p>
+            <p>
+            <small :class="{'text-success': account.active, 'text-danger': !account.active}">
+              {{ "activation  " + account.active }}
+            </small>
+          </p>
+          </div>
+          <span class="price float-end">{{ account.absoluteLimit }}</span>
+        </div>
+        <div class="card-footer">
+          <!-- <button class="btn btn-warning" @click="editAccount(account.id)">Edit</button>&nbsp;&nbsp; -->
+          <button class="btn btn-danger" @click="deleteAccount(account.accountId)">Delete</button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+import axios from '../../axios-auth'
+  
+  export default {
+    name: "TransactionListItem",
+    props: {
+      transaction: Object,
+    },
+    methods: {
+      
+   
+    },
+  };
+  </script>
+  
+  <style>
+  </style>
