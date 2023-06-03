0c792c5179fb93c8abcd9d966d53202b644432c0
domik <domi.kaniecki@gmail.com>
2023-06-03 15:22:25 +0200

    Added some user pages (Create, Edit, View)
    All of the user data can be accessed.

---
+++ src/components/user/UserListItem.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xxl-3 p-2">
+      <div class="card user-card h-100">
+        <div class="card-body">
+          <div class="float-start">
+            <p>{{ user.firstName }} {{ user.lastName }}</p>
+            <p>
+              <small>{{ user.email }}</small>
+            </p>
+          </div>
+          <span class="float-end">{{ user.phoneNumber }}</span>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-warning" @click="editUser(user.userId)">Edit</button>&nbsp;&nbsp;
+          <button class="btn btn-danger" @click="deleteUser(user.userId)">Delete</button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  
+  export default {
+    name: "UserListItem",
+    props: {
+      user: Object,
+    },
+    methods: {
+      deleteUser(id) {
+        axios
+          .delete("http://localhost:8080/users/" + id)
+          .then((result) => {
+            console.log(result);
+            this.$emit("update");
+          })
+          .catch((error) => console.log(error));
+      },
+      editUser(id) {
+        this.$router.push('/edituser/' + id);
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue';
 import ProductList from '../components/products/ProductList.vue';
 import AccountList from '../components/account/AccountList.vue';
+import UserList from '../components/user/UserList.vue';
+import EditUser from '../components/user/EditUser.vue';
 import CreateProduct from '../components/products/CreateProduct.vue';
 import EditProduct from '../components/products/EditProduct.vue';
 import Login from '../components/Login.vue';
@@ -12,6 +14,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: Home },
+
+    { path: '/users', component: UserList },
+    { path: '/edituser/:id', component: EditUser, props: true  },
+
     { path: '/products', component: ProductList },
     { path: '/accounts', component: AccountList },
     { path: '/login', component: Login },
