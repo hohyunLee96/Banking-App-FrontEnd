2fc0a28f46a85992dc34224612ea723131f8e36c
hohyunLee <hhhhhhlee@naver.com>
2023-06-07 03:27:20 +0200

    fixed

--- src/components/user/UserList.vue
+++ src/components/user/UserList.vue
@@ -1,93 +1,3 @@
-<!-- <template>
-  <section>
-    <div class="admin-panel">
-      <AdminPanel />
-    </div>
-    <div class="container">
-      <h2 class="mt-3 mt-lg-5">Users</h2>
-      <div class="filter-bar mt-3">
-        <label for="filter">Filter:</label>
-        <select id="filter" v-model="filterOption" @change="applyFilter">
-          <option value="all">All Users</option>
-          <option value="withoutAccount">Without Account</option>
-        </select>
-      </div>
-      <button type="button" class="btn btn-primary mt-3" @click="this.$router.push('/createuser');">
-        Add User
-      </button>
-      <div class="row mt-3">
-        <user-list-item
-          v-for="user in filteredUsers"
-          :key="user.id"
-          :user="user"
-          @update="update"
-        />
-      </div>
-    </div>
-  </section>
-</template>
-
-<script>
-import axios from "../../axios-auth";
-import AdminPanel from "./../AdminPanel.vue";
-import UserListItem from "./UserListItem.vue";
-
-export default {
-  name: "UserList",
-  components: {
-    AdminPanel,
-    UserListItem,
-  },
-  data() {
-    return {
-      users: [],
-      filterOption: "all",
-    };
-  },
-  mounted() {
-    this.update();
-    // Check the query parameter on initial load
-    const hasAccountParam = this.$route.query.hasAccount;
-    if (hasAccountParam === "false") {
-      this.filterOption = "withoutAccount";
-    }
-  },
-  computed: {
-    filteredUsers() {
-      if (this.filterOption === "all") {
-        return this.users;
-      } else if (this.filterOption === "withoutAccount") {
-        return this.users.filter(user => !user.hasAccount);
-      }
-    },
-  },
-  methods: {
-    update() {
-      axios
-        .get("users")
-        .then((result) => {
-          console.log(result);
-          this.users = result.data;
-        })
-        .catch((error) => console.log(error));
-    },
-    applyFilter() {
-      if (this.filterOption === "all") {
-        // Remove the query parameter from the URL
-        this.$router.replace({ query: null });
-      } else if (this.filterOption === "withoutAccount") {
-        // Set the query parameter to filter users without an account
-        this.$router.replace({ query: { hasAccount: false } });
-      }
-      // Call the update method to fetch the latest data from the server
-      this.update();
-    },
-  },
-};
-</script>
-
-<style>
-</style> -->
 <template>
   <section>
     <div class="admin-panel">
@@ -108,12 +18,7 @@ export default {
         Add User
       </button>
       <div class="row mt-3">
-        <user-list-item
-          v-for="user in filteredUsers"
-          :key="user.id"
-          :user="user"
-          @update="update"
-        />
+        <user-list-item v-for="user in filteredUsers" :key="user.id" :user="user" @update="update" />
       </div>
     </div>
   </section>
@@ -147,18 +52,8 @@ export default {
   },
   methods: {
     update() {
-      let url = this.baseUrl;
-
-      if (this.filterOption === "withoutAccount") {
-        url += "?hasAccount=false";
-      } else if (this.filterOption === "withoutSavingsAccount") {
-        url += "?excludedAccountType=SAVINGS";
-      } else if (this.filterOption === "withoutCurrentAccount") {
-        url += "?excludedAccountType=CURRENT";
-      }
-
       axios
-        .get(url)
+        .get("users")
         .then((result) => {
           console.log(result);
           this.users = result.data;
@@ -166,16 +61,19 @@ export default {
         .catch((error) => console.log(error));
     },
     applyFilter() {
-      if (this.filterOption === "all") {
-        this.$router.replace({ query: null });
-      } else if (this.filterOption === "withoutAccount") {
-        this.$router.replace({ query: { hasAccount: "false" } });
-      } else if (this.filterOption === "withoutSavingsAccount") {
-        this.$router.replace({ query: { excludedAccountType: "SAVINGS" } });
-      } else if (this.filterOption === "withoutCurrentAccount") {
-        this.$router.replace({ query: { excludedAccountType: "CURRENT" } });
-      }
-      this.update();
+      const params = {
+        hasAccount: this.filterOption === "withoutAccount" ? false : undefined,
+        excludedAccountType:
+          this.filterOption === "withoutSavingsAccount" ? "SAVINGS" :
+            this.filterOption === "withoutCurrentAccount" ? "CURRENT" : undefined,
+      };
+      axios
+        .get("users", { params })
+        .then((result) => {
+          console.log(result);
+          this.users = result.data;
+        })
+        .catch((error) => console.log(error));
     },
   },
 };
