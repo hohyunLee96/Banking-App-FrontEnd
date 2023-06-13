26bd213eb93f2780df5f8b17c84b9bc438538c0b
hohyunLee <hhhhhhlee@naver.com>
2023-06-13 18:15:07 +0200

    changed

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -28,7 +28,7 @@
           </router-link>
         </li>
         <li class="nav-item">
-          <router-link to="/test" v-if="!isUserRoleEmployee()" class="nav-link" active-class="active">
+          <router-link to="/search" v-if="!isUserRoleEmployee()" class="nav-link" active-class="active">
             <i class="fas fa-search"></i> Search Iban
           </router-link>
         </li>
